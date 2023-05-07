28b7909e85ae9972256b54e425aec0caa04c69b2
Anonymous8620 <90447430+Anonymous8620@users.noreply.github.com>
2023-05-07 20:04:44 +0530

    Add files via upload

---
+++ php_ese_mse/student.sql
@@ -0,0 +1,4 @@
+-- Run this in mysql phpadmin panel.
+-- database name should be test and table name should be student
+
+CREATE TABLE student (student_id int, name_ varchar(255), mse_marks1 int, mse_marks2 int, mse_marks3 int, mse_marks4 int, ese_marks1 int, ese_marks2 int, ese_marks3 int, ese_marks4 int)
