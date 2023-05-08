7c6d9961a531ba9f2d0a568b03e289d1e33a6bf3
Anonymous8620 <90447430+Anonymous8620@users.noreply.github.com>
2023-05-08 09:41:39 +0530

    Add files via upload

---
+++ IT company/index.js
@@ -0,0 +1,21 @@
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+})
+
+
+
+function myFunction() {
+    var video = document.getElementById("myVideo");
+var btn = document.getElementById("myBtn");
+    if (video.paused) {
+      video.play();
+      btn.innerHTML = "Pause";
+    } else {
+      video.pause();
+      btn.innerHTML = "Play";
+    }
+  }
